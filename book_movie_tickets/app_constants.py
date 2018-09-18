06d18c7adc3262f3d5d1264a98f87b9609307741
Darryl Fernandes <darrylfernandes@gmail.com>
2018-09-18 22:27:33 +1000

    remove token reference

--- book_movie_tickets/app_constants.py
+++ book_movie_tickets/app_constants.py
@@ -2,5 +2,5 @@ BASE_URL = 'http://webjetapitest.azurewebsites.net'
 MOVIE_TICKET_PROVIDERS = ['cinemaworld', 'filmworld']
 START_ENDPOINT_ALL_MOVIES = '/api/{}/movies'
 START_ENDPOINT_MOVIE_DETAILS = '/api/{}/movie/{}'
-URL_HEADERS = {'x-access-token': 'sjd1HfkjU83ksdsm3802k'}
+URL_HEADERS = {'x-access-token': 'REPLACE THIS'}
 TIMEOUT = 10  # in seconds
